a87066c609060c1d9adf06d382f0054638d5c3bc
Thomas Drevon <thomas@sanity.io>
2019-11-11 15:02:22 +0100

    Single-page PDFs are all the rage these days

--- api/_lib/chromium.ts
+++ api/_lib/chromium.ts
@@ -1,6 +1,9 @@
 import {launch, Page} from 'puppeteer-core'
 import {getOptions} from './options'
 import {FileType} from './types'
+
+const width = 1050
+const height = 600
 let _page: Page | null
 
 async function getPage(isDev: boolean) {
@@ -15,13 +18,14 @@ async function getPage(isDev: boolean) {
 
 export async function getScreenshot(url: string, type: FileType, isDev: boolean) {
   const page = await getPage(isDev)
-  await page.setViewport({width: 1050, height: 600})
+  await page.setViewport({width, height})
   await page.goto(url)
   let file
   if (['png', 'jpeg'].includes(type)) {
     file = await page.screenshot({type: type === 'jpeg' ? 'jpeg' : 'png'})
   } else {
-    file = await page.pdf({width: '1050px', height: '600px'})
+    const paddedHeight = height + 2 // 2 px padding keeps the PDF to a single page
+    file = await page.pdf({width: `${width}px`, height: `${paddedHeight}px`, printBackground: true})
   }
   return file
 }
